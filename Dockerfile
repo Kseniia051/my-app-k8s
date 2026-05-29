dd11084fd0476c206e2f689ea5e9c681fcb8ea9e
kali <ksenia@kali.kali>
2026-05-29 13:15:13 +0300

    fix: explicit amd64 platform in Dockerfile for minikube compatibility

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20-alpine
+FROM --platform=linux/amd64 node:20-alpine
 
 WORKDIR /app
 
@@ -8,7 +8,5 @@ RUN npm ci
 COPY . .
 
 EXPOSE 3000
-
-ENTRYPOINT []
 
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -3,80 +3,135 @@ let path = require('path');
 let fs = require('fs');
 let MongoClient = require('mongodb').MongoClient;
 let bodyParser = require('body-parser');
+
+//  PROMETHEUS
+const client = require('prom-client');
+
 let app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+/* PROMETHEUS METRICS */
+
+// базовые метрики Node.js (CPU, память и т.д.)
+client.collectDefaultMetrics({ timeout: 5000 });
+
+// счётчик HTTP запросов
+const httpRequestsTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status_code']
+});
+
+// время ответа
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'HTTP request duration in seconds',
+  labelNames: ['method', 'route'],
+  buckets: [0.01, 0.05, 0.1, 0.3, 0.5, 1, 2, 5]
+});
+
+// middleware для метрик
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+
+  res.on('finish', () => {
+    httpRequestsTotal.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode
+    });
+
+    end({ method: req.method, route: req.path });
   });
+
+  next();
+});
+
+// endpoint для Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+/* ТВОЁ ПРИЛОЖЕНИЕ */
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get('/profile-picture', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-// use when starting application locally with node command
+// MongoDB URLs
 let mongoUrlLocal = "mongodb://admin:password@mongodb:27017";
-
-// use when starting application as docker container, part of docker-compose
 let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
 
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+let mongoClientOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
-// "user-account" in demo with docker
 let databaseName = "user-account";
 let collectionName = "users";
 
+/* GET PROFILE */
+
 app.get('/get-profile', function (req, res) {
   let response = {};
-  // Connect to the db using local application or docker compose variable in connection properties
+
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
-
     let myquery = { userid: 1 };
 
     db.collection(collectionName).findOne(myquery, function (err, result) {
       if (err) throw err;
+
       response = result;
       client.close();
 
-      // Send response
       res.send(response ? response : {});
     });
   });
 });
 
+/* UPDATE PROFILE */
+
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
-  // Connect to the db using local application or docker compose variable in connection properties
+
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
+
     userObj['userid'] = 1;
 
     let myquery = { userid: 1 };
     let newvalues = { $set: userObj };
 
-    db.collection(collectionName).updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
-    });
-
+    db.collection(collectionName).updateOne(
+      myquery,
+      newvalues,
+      { upsert: true },
+      function (err, result) {
+        if (err) throw err;
+        client.close();
+      }
+    );
   });
-  // Send response
+
   res.send(userObj);
 });
+
+/*START SERVER */
 
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
-
